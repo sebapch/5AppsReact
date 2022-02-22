700e01742ea3cafad313fd6878408d2378014ec1
Sebastian Medina <snmedina91@gmail.com>
2022-02-22 16:52:24 -0300

    add welcome

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import LogoFondoClaro from '../assets/LogoFondoClaro'
 
 const Welcome = () => {
   return (
+      <>
     <div>Welcome</div>
+    <img src={LogoFondoClaro} alt='' className='banner-welcome'/>
+    </>
   )
 }
 
