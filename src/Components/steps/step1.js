ed26c3183710016a06cfd5a651dd006b6a944f2d
Sebastian Medina <snmedina91@gmail.com>
2022-02-25 02:42:59 -0300

    add carrousel

--- src/Components/steps/step1.js
+++ src/Components/steps/step1.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Paso1 from './Paso1'
+import Carrousel from './Carrousel'
 
 const Step1 = () => {
   return (
       <>
         <Paso1 />
         <h1 className='title'>Elegi el tipo de Vault que quieras armar</h1>
+        <Carrousel />
+        <h4 className='info'>Info</h4>
 
-    </>
-  )
-}
+            </>
+  );
+};
 
 export default Step1;
