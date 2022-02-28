a649816d9ebef0600b0ad00616247767ad357141
Sebastian Medina <39243497+sebapch@users.noreply.github.com>
2022-02-28 18:59:20 -0300

    remove paddings

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
             <div class="section">
                 <div class="container">
                     <div class="row full-height justify-content-center">
-                        <div class="col-12 text-center align-self-center py-5">
+                        <div class="col-12 text-center align-self-center ">
                             <div class="section pb-5 pt-5 pt-sm-2 text-center">
                                 <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
                                 <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
