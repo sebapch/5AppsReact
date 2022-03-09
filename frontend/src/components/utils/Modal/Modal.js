0ffccad9163a4033327855c7a729722db7148b45
Sebastian Medina <snmedina91@gmail.com>
2022-03-09 16:35:04 -0300

    add Modal

--- frontend/src/components/utils/Modal/Modal.js
+++ frontend/src/components/utils/Modal/Modal.js
@@ -1,34 +1,45 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import "./Modal.css" 
 
-const ModalInfo = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '100%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  inlineSize: "min-content",
+};
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
+export default function BasicModal({success, setSuccess, newName, link, newAddress}) {
+  
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-    };
-
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
-
-
-    return (
-        <>
-            <Button type="primary" onClick={showModal}>
-                Open Modal
-            </Button>
-            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-            </Modal>
-        </>
-    )
+  return (
+    <div className="modal-nuevo">
+      <Modal
+        open={success}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}  className="modal-content">
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Token successfully created!
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 3, mb: 3 }} >
+            You can see your Token "{newName}" at: <a href={link + newAddress}target="_blank" rel="noreferrer" style={{color: 'blue'}}>{link + newAddress}</a>
+          </Typography>
+          <Button variant="contained" id="modal" color="success" onClick={() => setSuccess(false)} className='modal-button'>
+            Accept
+          </Button>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
-
-export default ModalInfo
