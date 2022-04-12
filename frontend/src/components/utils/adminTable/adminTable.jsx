794236f532d19998bdd8e428791718d4064ab925
Sebastian Medina <snmedina91@gmail.com>
2022-04-11 21:48:31 -0300

    add Admin Panel brings data

--- frontend/src/components/utils/adminTable/adminTable.jsx
+++ frontend/src/components/utils/adminTable/adminTable.jsx
@@ -7,6 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Axios from "axios";
+import zIndex from '@mui/material/styles/zIndex';
+import { Button } from '@mui/material';
 
 
 function createData(name, calories, fat, carbs, protein) {
@@ -34,7 +36,7 @@ export default function BasicTable() {
     Axios.get("http://localhost:5000/users/api/vaults")
             .then(res => {
               console.log(res)
-              console.log(res.data[20].displayName)
+              
               setUsers(res)
             })
             .catch(err => {
@@ -45,7 +47,10 @@ export default function BasicTable() {
 
   useEffect(() => {
     traerVaults();
+    
   }, []);
+
+  console.log(users.data);
 
   
 
@@ -54,26 +59,33 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">UserName</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Saldo</TableCell>
+            <TableCell align="right">Vaults</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {users.data?.map((user, index) => (
             <TableRow
-              key={row.name}
+              key={index}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {user?._id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+               <TableCell align="right">{user?.displayName}</TableCell>
+              <TableCell align="right">{user?.email}</TableCell>
+              <TableCell align="right">{user?.saldo}</TableCell>
+             <TableCell align="right">{JSON.stringify(user?.vaults)}</TableCell>
+             <TableCell align="right">
+               <Button color="secondary">Editar</Button>
+               <Button color="error">Eliminar</Button>
+             </TableCell>
+
+              
             </TableRow>
           ))}
         </TableBody>
