851d9a822c6f32710673820354329f48816a705f
Sebastian Medina <snmedina91@gmail.com>
2022-04-15 15:36:51 -0300

    test

---
+++ frontend/build/precache-manifest.d49f9b612a6a6849a745f46123888e8b.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d044ddc5d02dad1d7ffad91872857fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1321a554ffd6b31e4be",
+    "url": "/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "c2182000afbcb519b2e5",
+    "url": "/static/css/main.faa9272b.chunk.css"
+  },
+  {
+    "revision": "c1321a554ffd6b31e4be",
+    "url": "/static/js/2.0a129c1e.chunk.js"
+  },
+  {
+    "revision": "ebe6c1cc4998babe3a771b9727186fb7",
+    "url": "/static/js/2.0a129c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2182000afbcb519b2e5",
+    "url": "/static/js/main.3dc09f0e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4b5e3914f41ea477de12c123e52c8462",
+    "url": "/static/media/7.4b5e3914.png"
+  },
+  {
+    "revision": "6ede42c055a387c85bb4a04866de482d",
+    "url": "/static/media/ACS.6ede42c0.svg"
+  },
+  {
+    "revision": "7ef61cedbb0cb9b1cbc54fbc4bb31af5",
+    "url": "/static/media/BNB.7ef61ced.svg"
+  },
+  {
+    "revision": "d2cf58e4a02b59ad2107e7413cb6148d",
+    "url": "/static/media/BSC.d2cf58e4.png"
+  },
+  {
+    "revision": "a6a702b5dbb8a4f212fccc91a1865bd5",
+    "url": "/static/media/BTC.a6a702b5.svg"
+  },
+  {
+    "revision": "229da97d108a6b50a594819202dd0061",
+    "url": "/static/media/Big4.229da97d.png"
+  },
+  {
+    "revision": "859f1e183d36fa05e8877c056f06333f",
+    "url": "/static/media/Estable.859f1e18.png"
+  },
+  {
+    "revision": "3a1c17f7180de31fa17809168db7670d",
+    "url": "/static/media/Ethereum.3a1c17f7.svg"
+  },
+  {
+    "revision": "c97371fdfaef20eb1c75edff8025f147",
+    "url": "/static/media/Instagram.c97371fd.svg"
+  },
+  {
+    "revision": "3e79d86ac052189787665f89b32ee1e0",
+    "url": "/static/media/LogoOficial.3e79d86a.svg"
+  },
+  {
+    "revision": "368929a5b6a6805c4be797bccf40905d",
+    "url": "/static/media/LogoSinTexto.368929a5.svg"
+  },
+  {
+    "revision": "392492e6db96f09ebc455f7971484ad3",
+    "url": "/static/media/Mascota.392492e6.svg"
+  },
+  {
+    "revision": "6aaf533633ddc8beadc5806d5a7da5ac",
+    "url": "/static/media/Telegram.6aaf5336.svg"
+  },
+  {
+    "revision": "fbeb045e909f5889f29bd981c2a3f6eb",
+    "url": "/static/media/Twitter.fbeb045e.svg"
+  },
+  {
+    "revision": "d6699f77a60d72d7c95b846b7837139c",
+    "url": "/static/media/UST.d6699f77.svg"
+  },
+  {
+    "revision": "ec2e5f9174e7796a09c6984ae16fd4af",
+    "url": "/static/media/Wallet.ec2e5f91.svg"
+  },
+  {
+    "revision": "68d9ed86cfe71e28e79ad8be35ed0fe0",
+    "url": "/static/media/Whatsapp.68d9ed86.svg"
+  },
+  {
+    "revision": "fd1545f29c140ae33d5f1d9ba1b35c61",
+    "url": "/static/media/ajustesBlanco.fd1545f2.svg"
+  },
+  {
+    "revision": "3f97f5f1abdc3b7064b5f1543f587383",
+    "url": "/static/media/ajustesVioleta.3f97f5f1.svg"
+  },
+  {
+    "revision": "c89f14504e1c6cd5b1c7380307ce6d46",
+    "url": "/static/media/bag.c89f1450.svg"
+  },
+  {
+    "revision": "5c763dec3081a6c55b0119c5b7890c99",
+    "url": "/static/media/cEURO.5c763dec.svg"
+  },
+  {
+    "revision": "3ec061c45318f6e6ceae8551b6066700",
+    "url": "/static/media/candado.3ec061c4.png"
+  },
+  {
+    "revision": "f8658af1ae096586a6d0b7c781dc7b69",
+    "url": "/static/media/guuruVioleta.f8658af1.svg"
+  },
+  {
+    "revision": "37c127ab33f1811ac011702c1ca62969",
+    "url": "/static/media/homeBlanco.37c127ab.svg"
+  },
+  {
+    "revision": "2c37891690b7fd96d36ebbfd61717683",
+    "url": "/static/media/homeVioleta.2c378916.svg"
+  },
+  {
+    "revision": "7fcbbc4d177985caf54b166fc76188a3",
+    "url": "/static/media/infoAzul.7fcbbc4d.svg"
+  },
+  {
+    "revision": "ac772775592ef1f393c3c3a233bbb9fd",
+    "url": "/static/media/infoVerde.ac772775.svg"
+  },
+  {
+    "revision": "0e39b0d9c3a3d8bcf0035b543611158c",
+    "url": "/static/media/infoVioleta.0e39b0d9.svg"
+  },
+  {
+    "revision": "e7a19b6dc5fc43c6023e80a324f21f94",
+    "url": "/static/media/lunaVioleta.e7a19b6d.svg"
+  },
+  {
+    "revision": "3b786022c015c8f2fa13f4c919300611",
+    "url": "/static/media/solBlanco.3b786022.svg"
+  },
+  {
+    "revision": "b0252de9ff092a483aa9d3b3b57bba55",
+    "url": "/static/media/usuarioBlanco.b0252de9.svg"
+  },
+  {
+    "revision": "1242f14a1c475e8f460296ee3a5c4311",
+    "url": "/static/media/usuarioVioleta.1242f14a.svg"
+  },
+  {
+    "revision": "17492aaca2985595c5b86f6633b51aee",
+    "url": "/static/media/vaultsBlanco.17492aac.svg"
+  },
+  {
+    "revision": "487d36eb95463a3aec1c11e5928fb3ec",
+    "url": "/static/media/vaultsVioleta.487d36eb.svg"
+  }
+]);
