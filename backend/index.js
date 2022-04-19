4323c828c846a5962c925e39414f2a5ca8e4e491
Sebastian Medina <snmedina91@gmail.com>
2022-04-19 00:01:06 -0300

    heroku commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 5000;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
 
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
