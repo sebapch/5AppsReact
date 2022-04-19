1c71730a4b61bee46ee3fc95e39043b75d8728d7
Sebastian Medina <snmedina91@gmail.com>
2022-04-18 21:44:51 -0300

    test aws

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,6 @@ app.use(cors());
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
